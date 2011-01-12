a1445a34d0990b69780a7144c77a9b21cfbcab8e
brettdavies <brettdavies@d060c8af-330a-42bd-b50c-f041eb749d1c>
2011-01-12 11:58:02 +0000

    Updated Validation on Contact Us form, created hooks for client side validation for custom validators

--- KSULax/Models/Forms.cs
+++ KSULax/Models/Forms.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace KSULax.Models
 {
@@ -12,321 +15,113 @@ namespace KSULax.Models
         public string Content { get; set; }
     }
 
-    #region FormTemplate
     /// <summary>
-    /// Template Class for forms. Provides required field checking.
+    /// Provides a Validator for Email Address fields.
     /// </summary>
-    public class FormTemplateModel : IDataErrorInfo
+    public class EmailAttribute : ValidationAttribute
     {
-        /// <summary>
-        /// Used to store errors. Format is ([Column name], [Error message])
-        /// </summary>
-        protected Dictionary<string, string> _errors = new Dictionary<string, string>();
-
-        public string Error
-        { get { return null; } }
-
-        public string this[string columnName]
+        public override bool IsValid(object address)
         {
-            get
+            if (address == null)
             {
-                string error;
-                if (_errors.TryGetValue(columnName, out error))
-                { return error; }
-                else
-                { return null; }
+                return true;
             }
+            try
+            {
+                address = new MailAddress(address.ToString()).Address;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+            return true;
         }
     }
-    #endregion FormTemplate
 
-    #region ContactUsForm
-    public class ContactUsFormModel : FormTemplateModel
+    public class EmailValidator : DataAnnotationsModelValidator<EmailAttribute>
     {
-        private string _subject;
-        public string Subject
+        string _message;
+
+        public EmailValidator(ModelMetadata metadata, ControllerContext context, EmailAttribute attribute)
+            : base(metadata, context, attribute)
         {
-            get { return _subject; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _subject = value; }
-                else
-                { _errors.Add("Subject", "'Subject' is required."); }
-            }
+            _message = attribute.ErrorMessage;
         }
 
-        private string _name;
-        public string Name
+        public override IEnumerable<ModelClientValidationRule>
+         GetClientValidationRules()
         {
-            get { return _name; }
-            set
+            var rule = new ModelClientValidationRule
             {
-                if (!string.IsNullOrEmpty(value))
-                { _name = value; }
-                else
-                { _errors.Add("Name", "'Name' is required."); }
-            }
-        }
-
-        private string _companyname;
-        public string CompanyName
-        {
-            get { return _companyname; }
-            set { _companyname = value; }
-        }
-
-        private string _email;
-        public string Email
-        {
-            get { return _email; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _email = value; }
-                else
-                { _errors.Add("Email", "'Email' is required."); }
-            }
-        }
-
-        private string _phone;
-        public string Phone
-        {
-            get { return _phone; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _phone = value; }
-                else
-                { _errors.Add("Phone", "'Phone' is required."); }
-            }
-        }
-
-        private string _comments;
-        public string Comments
-        {
-            get { return _comments; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _comments = value; }
-                else
-                { _errors.Add("Comments", "'Comments' is required."); }
-            }
+                ErrorMessage = _message,
+                ValidationType = "email"
+            };
+            return new[] { rule };
         }
     }
-    #endregion ContactUsForm
 
-    #region PlayerRegistrationForm
-    public class PlayerRegistrationModel : FormTemplateModel
+    /// <summary>
+    /// Class used on the Contact Us Form
+    /// </summary>
+    public class ContactUsFormModel
     {
-        private string _subject;
-        public string Subject
-        {
-            get { return _subject; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _subject = value; }
-                else
-                { _errors.Add("Subject", "'Subject' is required."); }
-            }
-        }
+        [Required(ErrorMessage = "Subject is Required.")]
+        public string Subject { get; set; }
 
-        private string _firstname;
-        public string FirstName
-        {
-            get { return _firstname; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _firstname = value; }
-                else
-                { _errors.Add("FirstName", "'First Name' is required."); }
-            }
-        }
+        [Required(ErrorMessage = "Name is Required.")]
+        [RegularExpression("^[a-zA-Z]+( [a-zA-Z]+)+$", ErrorMessage = "First and Last Name are Required")]
+        public string Name { get; set; }
 
-        private string _lastname;
-        public string LastName
-        {
-            get { return _lastname; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _lastname = value; }
-                else
-                { _errors.Add("LastName", "'Last Name' is required."); }
-            }
-        }
+        public string CompanyName { get; set; }
 
-        private string _phonenumber;
-        public string PhoneNumber
-        {
-            get { return _phonenumber; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _phonenumber = value; }
-                else
-                { _errors.Add("PhoneNumber", "'Phone Number' is required."); }
-            }
-        }
+        [Required(ErrorMessage = "Email is Required.")]
+        [Email(ErrorMessage = "Not a valid email.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
 
-        private string _email;
-        public string Email
-        {
-            get { return _email; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _email = value; }
-                else
-                { _errors.Add("Email", "'Email' is required."); }
-            }
-        }
+        [Required(ErrorMessage = "Phone is Required.")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
 
-        private string _uslaxid;
-        public string USLaxID
-        {
-            get { return _uslaxid; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _uslaxid = value; }
-                else
-                { _errors.Add("USLaxID", "'USLacrosse ID' is required."); }
-            }
-        }
-
-        private string _ksuid;
-        public string KSUID
-        {
-            get { return _ksuid; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _ksuid = value; }
-                else
-                { _errors.Add("KSUID", "'KSU ID' is required."); }
-            }
-        }
-
-        private string _major;
-        public string Major
-        {
-            get { return _major; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _major = value; }
-                else
-                { _errors.Add("Major", "'Major' is required."); }
-            }
-        }
-
-        private string _gpa;
-        public string GPA
-        {
-            get { return _gpa; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _gpa = value; }
-                else
-                { _errors.Add("GPA", "'GPA' is required."); }
-            }
-        }
-
-        private string _height;
-        public string Height
-        {
-            get { return _height; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _height = value; }
-                else
-                { _errors.Add("Height", "'Height' is required."); }
-            }
-        }
-
-        private string _weight;
-        public string Weight
-        {
-            get { return _weight; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _weight = value; }
-                else
-                { _errors.Add("Weight", "'Weight' is required."); }
-            }
-        }
-
-        private string _position;
-        public string Position
-        {
-            get { return _position; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _position = value; }
-                else
-                { _errors.Add("Position", "'Position' is required."); }
-            }
-        }
-
-        private string _hometown;
-        public string Hometown
-        {
-            get { return _hometown; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _hometown = value; }
-                else
-                { _errors.Add("Hometown", "'Hometown' is required."); }
-            }
-        }
-
-        private string _homestate;
-        public string Homestate
-        {
-            get { return _homestate; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _homestate = value; }
-                else
-                { _errors.Add("Homestate", "'Homestate' is required."); }
-            }
-        }
-
-        private string _highschool;
-        public string Highschool
-        {
-            get { return _highschool; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _highschool = value; }
-                else
-                { _errors.Add("Highschool", "'Highschool' is required."); }
-            }
-        }
-
-        private string _highschoolyear;
-        public string Highschoolyear
-        {
-            get { return _highschoolyear; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                { _highschoolyear = value; }
-                else
-                { _errors.Add("Highschoolyear", "'Highschool Year' is required."); }
-            }
-        }
+        [Required(ErrorMessage = "A Message is Required.")]
+        public string Comments { get; set; }
     }
-    #endregion PlayerRegistrationForm
+
+    /// <summary>
+    /// Class used on the Player Registration Form
+    /// </summary>
+    public class PlayerRegistrationModel
+    {
+        public string Subject { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string USLaxID { get; set; }
+
+        public string KSUID { get; set; }
+
+        public string Major { get; set; }
+
+        public string GPA { get; set; }
+
+        public string Height { get; set; }
+
+        public string Weight { get; set; }
+
+        public string Position { get; set; }
+
+        public string Hometown { get; set; }
+
+        public string Homestate { get; set; }
+
+        public string Highschool { get; set; }
+
+        public string Highschoolyear { get; set; }
+    }
 }
--- KSULax/Views/Forms/Contact-Us.aspx
+++ KSULax/Views/Forms/Contact-Us.aspx
@@ -1,5 +1,25 @@
 <%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<KSULax.Models.ContactUsFormModel>" %>
 <asp:Content ContentPlaceHolderID="titleContent" runat="server">Contact Us</asp:Content>
+<asp:Content ContentPlaceHolderID="Header" runat="server">
+<script src="../../content/scripts/MicrosoftAjax.js" type="text/javascript"></script>
+<script src="../../content/scripts/MicrosoftMvcValidation.js" type="text/javascript"></script>
+<script type="text/javascript">
+    Sys.Mvc.ValidatorRegistry.validators["email"] = function (rule) {
+        // initialization code can go here.
+        var filter = /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-zA-Z0-9]{1}[a-zA-Z0-9\-]{0,62}[a-zA-Z0-9]{1})|[a-zA-Z])\.)+[a-zA-Z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/;
+
+        // we return the function that actually does the validation 
+        return function (value, context) {
+            if (filter.test(value)) {
+                return true;
+            }
+            return rule.ErrorMessage;
+        };
+    };
+</script>
+
+
+</asp:Content>
 <asp:Content ContentPlaceHolderID="MainContent" runat="server">
 <div id="leftCol">
 <% Html.RenderPartial("SponsorsTemplate"); %>
@@ -8,51 +28,52 @@
 <div class="breadcrumbs"><%= Html.ActionLink("Home", "", "", null, new { title = "Home" })%> > <%= Html.ActionLink("Forms", "Index", null, null, new { title = "Forms" })%> > <%= Html.ActionLink("Contact Us", "contact-us", null, new { title = "Contact Us" })%></div>
 <h1>Contact Us</h1>
 <p>Kennesaw State University Lacrosse extends the opportunity for upcoming freshmen or transfer students enrolling at KSU to join the team. All players must be students of Kennesaw State University (no exceptions). If you are interested in participating in this program, please fill out the form below to receive more information. If you are not a student of the university, but would like to attend, please go to <a href="http://www.kennesaw.edu/admissions/ugadm.shtml" title="KSU Admissions">admissions</a> to apply.</p>
-<%= Html.ValidationSummary("Please correct the following errors and try again.") %>
+<% Html.EnableClientValidation(); %>
 <% using (Html.BeginForm("contact-us", "forms", FormMethod.Post, new { id = "email_form" })) {%>
+<%= Html.ValidationSummary(true) %>
 <table style="width:100%;" cellpadding="3">
 <tr>
-<td style="width:16%;" valign="middle" class="statId"><div style="text-align:right;">Subject:</div></td>
+<td style="width:16%;" valign="middle" class="statId"><div style="text-align:right;"><%= Html.LabelFor(model => model.Subject) %></div></td>
 <td style="width:84%;" valign="top">
-<%= Html.TextBox("Subject")%>
-<%= Html.ValidationMessage("Subject", "*")%>
+<%= Html.TextBoxFor(model => model.Subject) %>
+<%= Html.ValidationMessageFor(model => model.Subject) %>
 </td>
 </tr>
 <tr>
 <td valign="middle" class="statId">
-<div style="text-align:right;">Name:</div>   
+<div style="text-align:right;"><%= Html.LabelFor(model => model.Name) %></div>   
 </td>
 <td valign="top">
-<%= Html.TextBox("Name")%>
-<%= Html.ValidationMessage("Name", "*")%>
+<%= Html.TextBoxFor(model => model.Name) %>
+<%= Html.ValidationMessageFor(model => model.Name) %>
 </td>
 </tr>
 <tr>
-<td valign="middle" class="statId"><div style="text-align:right;">Company or School:</div></td>
+<td valign="middle" class="statId"><div style="text-align:right;"><label for="CompanyName">Company or School</label></div></td>
 <td valign="top">
-<%= Html.TextBox("CompanyName")%>
+<%= Html.TextBoxFor(model => model.CompanyName) %>
+<%= Html.ValidationMessageFor(model => model.CompanyName) %>
 </td>
 </tr>
 <tr>
-<td valign="middle" class="statId"><div style="text-align:right;">Email:</div></td>
+<td valign="middle" class="statId"><div style="text-align:right;"><%= Html.LabelFor(model => model.Email) %></div></td>
 <td valign="top">
-<%= Html.TextBox("Email")%>
-<%= Html.ValidationMessage("Email", "*")%>
+<%= Html.TextBoxFor(model => model.Email) %>
+<%= Html.ValidationMessageFor(model => model.Email) %>
 </td>
 </tr>
 <tr>
-<td valign="middle" class="statId"><div style="text-align:right;">Phone:</div></td>
+<td valign="middle" class="statId"><div style="text-align:right;"><%= Html.LabelFor(model => model.Phone) %></div></td>
 <td valign="top">
-<%= Html.TextBox("Phone")%>
-<%= Html.ValidationMessage("Phone", "*")%>
+<%= Html.TextBoxFor(model => model.Phone) %>
+<%= Html.ValidationMessageFor(model => model.Phone) %>
 </td>
 </tr>
 <tr>
-<td valign="top" class="statId"><div style="text-align:right;">Comments:</div></td>
+<td valign="top" class="statId"><div style="text-align:right;"><%= Html.LabelFor(model => model.Comments) %></div></td>
 <td valign="top">
-<%= Html.TextArea("comments", new { @rows = "10", @cols = "40" })%>
-<%= Html.ValidationMessage("Comments", "*")%>
-</td>
+<%= Html.TextAreaFor(model => model.Comments, new { @rows = "10", @cols = "40" })%>
+<%= Html.ValidationMessageFor(model => model.Comments) %></td>
 </tr>
 <tr>
 <td valign="top">&nbsp;</td>
