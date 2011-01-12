88c609d0f3f979b210a882f117760805789c6240
Brett <brettdavies@users.noreply.github.com>
2011-01-12 11:59:04 +0000

    demo on how to validate the entity framework database models

--- Models/CustomClasses.cs
+++ Models/CustomClasses.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace KSULax.Models
 {
@@ -25,4 +27,25 @@ namespace KSULax.Models
         public short rank { get; set; }
         public string rankUrl { get; set; }
     }
+
+
+    #region Database Model Validations
+
+    [MetadataType(typeof(Photographer_Validation))]
+    public partial class Photographer { }
+
+    [Bind(Exclude = "photographer_id")]
+    public class Photographer_Validation
+    {
+        [Required(ErrorMessage = "Required")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string company { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string url { get; set; }
+    }
+
+    #endregion Database Model Validations
 }
