276d55f62fffe39bbff090f7e03fcd4179a74b70
Brett <brettdavies@users.noreply.github.com>
2011-01-18 08:54:47 +0000

    Created new Entity and Business Layer schema for News data. Templated views not working so this check-in contains mostly logic.

---
+++ Entities/NewsBE.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KSULax.Entities
+{
+    public class NewsBE
+    {
+        private string _title;
+        private string _titlePath;
+
+        /// <summary>
+        /// Date for a news story
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Title of a news story
+        /// </summary>
+        public string Title
+        {
+            get
+            {
+                return _title.Trim();
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+
+        /// <summary>
+        /// Path element of a news story
+        /// </summary>
+        public string TitlePath
+        {
+            get
+            {
+                return _titlePath.ToLower();
+            }
+
+            set
+            {
+                _titlePath = value;
+            }
+        }
+        
+        /// <summary>
+        /// Text of news story
+        /// </summary>
+        public string Story { get; set; }
+
+        /// <summary>
+        /// Author of a news story
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Source of a news story
+        /// </summary>
+        public string Source { get; set; }
+
+        /// <summary>
+        /// URL of the source of a news story
+        /// </summary>
+        public string SourceUrl { get; set; }
+    }
+}
---
+++ Models/News/TemplateModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KSULax.Entities;
+
+namespace KSULax.Models.News
+{
+    public class TemplateModel
+    {
+        public TemplateModel(NewsBE news, string requestUrl)
+        {
+            Author = news.Author;
+            Date = news.Date;
+            Source = news.Source;
+
+            if (String.IsNullOrEmpty(news.SourceUrl))
+            {
+                SourceUrl = requestUrl;
+            }
+            else
+            {
+                SourceUrl = news.SourceUrl;
+            }
+            
+            Story = news.Story;
+            Title = news.Title;
+            TitlePath = news.TitlePath;
+        }
+
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string TitlePath { get; set; }
+        public string Story { get; set; }
+        public string Author { get; set; }
+        public string Source { get; set; }
+        public string SourceUrl { get; set; }
+    }
+}
