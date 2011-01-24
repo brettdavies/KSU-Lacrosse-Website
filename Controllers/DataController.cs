691afd1f0993a0d8ebad81cc1e8aa1fe2b807d83
Brett <brettdavies@users.noreply.github.com>
2011-01-24 08:31:51 +0000

    Created Data BE / BL / Controller. Pulled GetRanking out of Home Controller. Updated ksulax.js to reflect new address of jsonRequest

---
+++ Controllers/DataController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KSULax.Models;
+using KSULax.Logic;
+using KSULax.Entities;
+
+namespace KSULax.Controllers
+{
+    public class DataController : Controller
+    {
+        private KSULaxEntities _entities;
+        private DataBL _dataBL;
+
+        public DataController()
+        {
+            _entities = new KSULaxEntities();
+            _dataBL = new DataBL(_entities);
+        }
+
+        public ActionResult Index()
+        {
+            return null;
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public JsonResult GetRanking()
+        {
+            return this.Json(_dataBL.GetRanking(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/Data/RankingModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KSULax.Models.Data
+{
+    public class RankingModel
+    {
+    }
+}
