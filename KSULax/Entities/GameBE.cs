521f32145726b4f454524c3763ad380c6e41b71e
brettdavies <brettdavies@d060c8af-330a-42bd-b50c-f041eb749d1c>
2011-01-24 08:42:50 +0000

    Created Game BE / BL. Still lots of work to do.

---
+++ KSULax/Entities/GameBE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KSULax.Entities
+{
+    class GameBE
+    {
+        public int ID { get; set; }
+        public int SeasonID { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public string Venue { get; set; }
+        public string Detail { get; set; }
+        public int AwayTeamScore { get; set; }
+        public int HomeTeamScore { get; set; }
+        public TeamBE AwayTeam { get; set; }
+        public TeamBE HomeTeam { get; set; }
+        public List<PhotoGalleryBE> PhotoGallery { get; set; }
+        public List<PhotoBE> Photo { get; set; }
+        public List<PlayerGameBE> PlayerGame { get; set; }
+    }
+}
---
+++ KSULax/Logic/GameBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KSULax.Entities;
+using KSULax.Models;
+using KSULax.Controllers;
+
+namespace KSULax.Logic
+{
+    public class GameBL
+    {
+        KSULaxEntities _entities;
+
+        public GameBL(KSULaxEntities entitity) { _entities = entitity; }
+
+    }
+}
