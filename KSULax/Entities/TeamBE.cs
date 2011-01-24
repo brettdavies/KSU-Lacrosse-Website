a99a64286b7e6f528f6acfbe5f1f9666b24fedac
brettdavies <brettdavies@d060c8af-330a-42bd-b50c-f041eb749d1c>
2011-01-24 08:43:09 +0000

    Created Team BE.

---
+++ KSULax/Entities/TeamBE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KSULax.Entities
+{
+    class TeamBE
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Abr { get; set; }
+        public string Slug { get; set; }
+        public Uri TeamURL { get; set; }
+        public string Mascot { get; set; }
+        public List<GameBE> AwayGame { get; set; }
+        public List<GameBE> HomeGame { get; set; }
+    }
+}
