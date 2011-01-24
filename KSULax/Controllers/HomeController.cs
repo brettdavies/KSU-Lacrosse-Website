8e6669eb8ee46dade46b740ce7f41ff9e1ab97c0
brettdavies <brettdavies@d060c8af-330a-42bd-b50c-f041eb749d1c>
2011-01-24 08:31:51 +0000

    Created Data BE / BL / Controller. Pulled GetRanking out of Home Controller. Updated ksulax.js to reflect new address of jsonRequest

--- KSULax/Controllers/HomeController.cs
+++ KSULax/Controllers/HomeController.cs
@@ -36,34 +36,6 @@ namespace KSULax.Controllers
             return View();
         }
 
-        [AcceptVerbs(HttpVerbs.Get)]
-        public JsonResult GetRanking()
-        {
-            List<Ranking> ranking = new List<Ranking>(3);
-            ranking.Add(RecentRankingbyPollID(1));
-            ranking[0].datestr = ranking[0].date.ToString("MMMM dd, yyyy");
-            ranking.Add(RecentRankingbyPollID(2));
-            ranking[1].datestr = ranking[1].date.ToString("MMMM dd, yyyy");
-            ranking.Add(RecentRankingbyPollID(3));
-            ranking[2].datestr = ranking[2].date.ToString("MMMM dd, yyyy");
-
-            return this.Json(ranking, JsonRequestBehavior.AllowGet);
-        }
-
-        private Ranking RecentRankingbyPollID(int pollID)
-        {
-            var result = from Rank in _entities.PollSet.Where("it.pollsource_id = " + pollID).OrderBy("it.date desc").Take(1)
-                         select new Ranking
-                         {
-                             pollSource = Rank.pollsource_id,
-                             date = Rank.date,
-                             rank = Rank.rank,
-                             rankUrl = Rank.url
-                         };
-
-            return result.First<Ranking>();
-        }
-
         protected List<StoryModel> GetStoryModels(List<NewsBE> news, string requestUrl)
         {
             var Stories = new List<StoryModel>();
---
+++ KSULax/Logic/DataBL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KSULax.Entities;
+using KSULax.Models;
+using KSULax.Controllers;
+
+namespace KSULax.Logic
+{
+    public class DataBL
+    {
+        KSULaxEntities _entities;
+
+        public DataBL(KSULaxEntities entitity) { _entities = entitity; }
+
+        /// <summary>
+        /// Returns the most recent Top 25 Ranking for the team.
+        /// </summary>
+        /// <returns></returns>
+        public List<TeamRankingBE> GetRanking()
+        {
+            List<TeamRankingBE> ranking = new List<TeamRankingBE>(3);
+            ranking.Add(RecentRankingbyPollID(1));
+            ranking[0].datestr = ranking[0].date.ToString("MMMM dd, yyyy");
+            ranking.Add(RecentRankingbyPollID(2));
+            ranking[1].datestr = ranking[1].date.ToString("MMMM dd, yyyy");
+            ranking.Add(RecentRankingbyPollID(3));
+            ranking[2].datestr = ranking[2].date.ToString("MMMM dd, yyyy");
+
+            return ranking;
+        }
+
+        private TeamRankingBE RecentRankingbyPollID(int pollID)
+        {
+            var result = from Rank in _entities.PollSet.Where("it.pollsource_id = " + pollID).OrderBy("it.date desc").Take(1)
+                         select new TeamRankingBE
+                         {
+                             pollSource = Rank.pollsource_id,
+                             date = Rank.date,
+                             rank = Rank.rank,
+                             rankUrl = Rank.url
+                         };
+
+            return result.First<TeamRankingBE>();
+        }
+    }
+}
