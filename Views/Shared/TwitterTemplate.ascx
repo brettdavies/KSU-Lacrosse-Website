4188e8e1440f92149e13a17ae1af38a499833d6b
Brett <brettdavies@users.noreply.github.com>
2014-01-26 11:36:06 +0000

    Deprecated old Twitter integration (v1) Added new timeline widget (v1.1)

--- Views/Shared/TwitterTemplate.ascx
+++ Views/Shared/TwitterTemplate.ascx
@@ -1,9 +1,9 @@
 <%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl" %>
-<script type="text/javascript">$(document).ready(getTweets);</script>
 <div class="boxItem">
-<div class="boxTitle"><a href="http://twitter.com/kstatelax" title="KSU Lax on Twitter">Twitter</a></div>
+<div class="boxTitle"><a href="http://twitter.com/KStateLax" title="KSU Lax on Twitter">Twitter</a></div>
 <div id="twitterBox" class="boxContent">
-<noscript>Javascript must be enabled to see Twitter posts</noscript>
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/KStateLax" data-widget-id="427400494017810432" data-link-color="#febc11" data-chrome="noheader nofooter noborders transparent" data-tweet-limit="3">Tweets by @KStateLax</a>
+<script>!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + "://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } } (document, "script", "twitter-wjs");</script>
 </div>
-<div class="boxBottom"><img src="../../content/images/gray-arrow.png" alt="" /><a href="http://twitter.com/kstatelax" title="KSU Lax on Twitter"> Follow KStateLax on Twitter</a></div>
+<div class="boxBottom"><img src="../../content/images/gray-arrow.png" alt="" /><a href="http://twitter.com/KStateLax" title="KSU Lax on Twitter"> Follow KStateLax on Twitter</a></div>
 </div>
--- content/scripts/twitter.js
+++ content/scripts/twitter.js
@@ -1,78 +1,78 @@
-﻿function twitterDates(date_str) {
-    var date = new Date(date_str);
-    date_str = dateFormat(date, 'isoUtcDateTime');
+﻿//function twitterDates(date_str) {
+//    var date = new Date(date_str);
+//    date_str = dateFormat(date, 'isoUtcDateTime');
 
-    var time_formats = [
-		[60, 'Just Now'],
-		[90, '1 Minute'],
-		[3600, 'Minutes', 60],
-		[5400, '1 Hour'],
-		[86400, 'Hours', 3600],
-		[129600, '1 Day'],
-		[604800, 'Days', 86400],
-		[907200, '1 Week'],
-		[2628000, 'Weeks', 604800]
-	];
+//    var time_formats = [
+//		[60, 'Just Now'],
+//		[90, '1 Minute'],
+//		[3600, 'Minutes', 60],
+//		[5400, '1 Hour'],
+//		[86400, 'Hours', 3600],
+//		[129600, '1 Day'],
+//		[604800, 'Days', 86400],
+//		[907200, '1 Week'],
+//		[2628000, 'Weeks', 604800]
+//	];
 
-    var time = ('' + date_str).replace(/-/g, "/").replace(/[TZ]/g, " "),
-		dt = new Date,
-		seconds = ((dt - new Date(time) + (dt.getTimezoneOffset() * 60000)) / 1000),
-		token = ' Ago',
-		i = 0,
-		format;
+//    var time = ('' + date_str).replace(/-/g, "/").replace(/[TZ]/g, " "),
+//		dt = new Date,
+//		seconds = ((dt - new Date(time) + (dt.getTimezoneOffset() * 60000)) / 1000),
+//		token = ' Ago',
+//		i = 0,
+//		format;
 
-    if (seconds < 0) {
-        seconds = Math.abs(seconds);
-        token = '';
-    }
+//    if (seconds < 0) {
+//        seconds = Math.abs(seconds);
+//        token = '';
+//    }
 
-    while (format = time_formats[i++]) {
-        if (seconds < format[0]) {
-            if (format.length == 2) {
-                return format[1] + (i > 1 ? token : '');
-            } else {
-                return Math.round(seconds / format[2]) + ' ' + format[1] + (i > 1 ? token : '');
-            }
-        }
-    }
+//    while (format = time_formats[i++]) {
+//        if (seconds < format[0]) {
+//            if (format.length == 2) {
+//                return format[1] + (i > 1 ? token : '');
+//            } else {
+//                return Math.round(seconds / format[2]) + ' ' + format[1] + (i > 1 ? token : '');
+//            }
+//        }
+//    }
 
-    var localdate = date.toLocaleDateString();
-    return localdate.substring(localdate.indexOf(',') + 2, localdate.length);
-};
+//    var localdate = date.toLocaleDateString();
+//    return localdate.substring(localdate.indexOf(',') + 2, localdate.length);
+//};
 
-String.prototype.parseURL = function() {
-    return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
-        return url.link(url);
-    });
-};
+//String.prototype.parseURL = function() {
+//    return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
+//        return url.link(url);
+//    });
+//};
 
-String.prototype.parseUsername = function() {
-    return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
-        var un = u.replace("@", "")
-        return u.link("http://twitter.com/" + un);
-    });
-};
+//String.prototype.parseUsername = function() {
+//    return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+//        var un = u.replace("@", "")
+//        return u.link("http://twitter.com/" + un);
+//    });
+//};
 
-String.prototype.parseHashtag = function() {
-    return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
-        var ht = t.replace("#", "%23")
-        return t.link("http://search.twitter.com/search?q=" + ht);
-    });
-};
+//String.prototype.parseHashtag = function() {
+//    return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+//        var ht = t.replace("#", "%23")
+//        return t.link("http://search.twitter.com/search?q=" + ht);
+//    });
+//};
 
-function getTweets() {
-    var url = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=kstatelax&skip_user=true&include_rts=true&count=2&callback=?';
-    var textToInsert = [];
-    $.getJSON(url, function(tweets) {
-        $.each(tweets, function(i, tweet) {
-            textToInsert.push('<div class="homeNewsItem">');
-            textToInsert.push('<div class="date"><a href="http://twitter.com/KStateLax/status/' + tweet.id + '">' + twitterDates((tweet.created_at).replace("+0000 ", "") + " GMT") + '</a></div>');
-            textToInsert.push('<p>' + tweet.text.parseURL().parseUsername().parseHashtag() + '</p></div>');
-        });
-        $('#twitterBox').html(textToInsert.join(''));
-        var links = $('#twitterBox a[href]');
-        for (var i = 0; i < links.length; i++) {
-            trackLinks(links[i]);
-        };
-    });
-};
+//function getTweets() {
+//    var url = 'http://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=kstatelax&skip_user=true&include_rts=true&count=2&callback=?';
+//    var textToInsert = [];
+//    $.getJSON(url, function(tweets) {
+//        $.each(tweets, function(i, tweet) {
+//            textToInsert.push('<div class="homeNewsItem">');
+//            textToInsert.push('<div class="date"><a href="http://twitter.com/KStateLax/status/' + tweet.id + '">' + twitterDates((tweet.created_at).replace("+0000 ", "") + " GMT") + '</a></div>');
+//            textToInsert.push('<p>' + tweet.text.parseURL().parseUsername().parseHashtag() + '</p></div>');
+//        });
+//        $('#twitterBox').html(textToInsert.join(''));
+//        var links = $('#twitterBox a[href]');
+//        for (var i = 0; i < links.length; i++) {
+//            trackLinks(links[i]);
+//        };
+//    });
+//};
